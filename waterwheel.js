9081fe09972499e28e880d2bc9bd07da744e217c
Adam Benzan <adam.benzan@gmail.com>
2013-11-24 08:14:23 -0500

    add width selector

--- waterwheel.js
+++ waterwheel.js
@@ -1,8 +1,6 @@
 /* global _*/
 
 (function (context) {
-  var lastDash = 0;
-
   var Bucket = function(angle, wheel) {
     this.wheelSize = wheel.canvas.width;
     this.wheel = wheel;
@@ -10,13 +8,13 @@
     this.angle = angle;
     this.initialize();
     this.__defineGetter__('width', function() {
-      return this._width + Math.sqrt(this.mass) / 3;
+      return this._width + this.mass / ((this.wheel.numBuckets+1)*15);
     });
     this.__defineSetter__('width', function(width) {
       this._width = width;
     });
     this.__defineGetter__('height', function() {
-      return this._height + Math.sqrt(this.mass) / 2;
+      return this._height + this.mass / ((this.wheel.numBuckets+1)*15);
     });
     this.__defineSetter__('height', function(height) {
       this._height = height;
@@ -32,8 +30,8 @@
       return Math.sin(this.angle) * this.mass * -1;
     },
     updatePosition: function() {
-      this.x = this.wheelSize * 0.5 + this.wheelSize * 0.45 * Math.sin(this.angle);
-      this.y = this.wheelSize * 0.5 + this.wheelSize * 0.45 * Math.cos(this.angle);
+      this.x = this.wheelSize * 0.5 + this.wheelSize * 0.4 * Math.sin(this.angle);
+      this.y = this.wheelSize * 0.5 + this.wheelSize * 0.4 * Math.cos(this.angle);
     },
     draw: function(ctx) {
       this.updatePosition();
@@ -43,7 +41,6 @@
         ctx.fillStyle = '#8ED6FF';
         ctx.strokeStyle = '#8ED6FF';
         ctx.beginPath();
-        ctx.setLineDash([5, lastDash % 5]);
         ctx.moveTo(this.x, this.y);
         ctx.lineTo(this.x, 0);
         ctx.lineWidth = (this.wheel.fillRate / 200.0) * 20;
@@ -54,18 +51,9 @@
         ctx.fillStyle = '#000000';
       }
 
-      ctx.setLineDash([0]);
-
-      ctx.beginPath();
-      ctx.moveTo(this.x, this.y);
-      ctx.lineTo(this.wheelSize/2, this.wheelSize/2);
-      ctx.lineWidth = 2;
-      ctx.stroke();
-
       if (this.mass > 0) {
         ctx.strokeStyle = '#8ED6FF';
         ctx.beginPath();
-        ctx.setLineDash([5, lastDash % 5]);
         ctx.moveTo(this.x, this.y);
         if (this.targetBucket) {
           ctx.lineTo(this.targetBucket.x, this.targetBucket.y);
@@ -85,24 +73,22 @@
   };
 
   var Wheel = function(numBuckets, fillRate, drainRate, canvas) {
-    this.numBuckets = numBuckets;
+    this.initialBuckets = numBuckets;
     this.fillRate = fillRate;
     this.drainRate = drainRate;
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
-    this.framesRendered = 0;
     this.initialize();
   }
 
   Wheel.prototype = {
+    friction: 0.01,
+    framesRenderer: 0,
     spoutPosition: 300,
     initialize: function() {
-      if (!this.ctx.setLineDash) {
-        this.ctx.setLineDash = function() {};
-      }
       _.bindAll(this, 'toggleAnimating', 'animate', 'draw');
       this.buckets = [];
-      for (var i = 0; i < this.numBuckets; i++) {
+      for (var i = 0; i < this.initialBuckets; i++) {
         this.addBucket();
       }
       this.draw();
@@ -114,14 +100,20 @@
         this.buckets[i].angle = i * angleDelta;
       }
       this.buckets.push(new Bucket((i) * angleDelta, this));
+      this.numBuckets = this.buckets.length;
     },
-
+    adjustBaseBucketWidth: function(newValue) {
+      for (var i = 0; i < this.buckets.length; i++) {
+        this.buckets[i]._width = newValue;
+      }
+    },
     removeBucket: function() {
       this.buckets = this.buckets.slice(1);
       var angleDelta = 2 * Math.PI / (this.buckets.length);
       for (var i = 0; i < this.buckets.length; i++) {
         this.buckets[i].angle = i * angleDelta;
       }
+      this.numBuckets = this.buckets.length;
     },
 
     toggleAnimating: function() {
@@ -173,6 +165,10 @@
       this.drainRate = newValue;
     },
 
+    setFriction: function(newValue) {
+      this.friction = newValue;
+    },
+
     animate: function() {
       var angularForce = 0;
       this.fillSpoutTarget();
@@ -180,6 +176,7 @@
         this.buckets[i].targetBucket = false;
         angularForce += this.buckets[i].getDownwardForce();
       }
+      angularForce *= 1 - this.friction;
       for (i = 0, l = this.buckets.length; i < l; i++) {
         this.buckets[i].angle += angularForce / (1000 * this.numBuckets);
         if (this.buckets[i].mass > this.drainRate) {
@@ -196,11 +193,24 @@
       }
       this.draw();
     },
+
+    drawSpokes: function() {
+      for (var i = 0, l = this.buckets.length; i < l; i++) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.buckets[i].x, this.buckets[i].y);
+        this.ctx.lineTo(this.canvas.width / 2, this.canvas.height / 2);
+        this.ctx.lineWidth = 2;
+        this.ctx.stroke();
+      }
+    },
     draw: function() {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
       for (var i = 0, l = this.buckets.length; i < l; i++) {
+
         this.buckets[i].draw(this.ctx);
       }
+      this.drawSpokes();
     }
 
   };
